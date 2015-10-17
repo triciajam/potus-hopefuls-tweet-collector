f5e8338734ffcfba648b13abfef84d668c7e0f3b
triciajam <tricia.jamison@gmail.com>
2015-10-16 23:33:50 -0400

    Added better error handling and output messages.

--- tc_cron_stop.sh
+++ tc_cron_stop.sh
@@ -16,32 +16,34 @@ pid=`cat ${data_dir}/pid_$1`
 kill -9 $pid
 echo "** $datetime [ TC-STOP ] : Ended $1 at $pid"
 
-{
-  echo "** $datetime [ TC-STOP ] : Removing last incomplete lines from files."
-  dirs=`ls -d $data_dir/*/`
-  lastfiles=( `for d in $dirs; do name=\`ls -t  $d | head -n1\`; echo $d$name ; done` ) # find very last files
-  echo "** $datetime [ TC-STOP ] : There are ${#lastfiles[@]} very last files written (in each folder)."  
 
-  chour=`date +"%H"`
-  cdate=`date +"%Y-%m-%d"`
-  lastfilesthishour=( `find ${lastfiles[@]} -type f -newerct "${cdate} ${chour}:00:00"` ) # confirm they were written this hour
-  echo "** $datetime [ TC-STOP ] : ${#lastfilesthishour[@]} very last files were written this hour."	
+dirs=`ls -d $data_dir/*/`
+lastfiles=( `for d in $dirs; do name=\`ls -t  $d | head -n1\`; echo $d$name ; done` ) # find very last files
+echo "** $datetime [ TC-STOP ] : There are ${#lastfiles[@]} last files written (in each folder)."  
 
-  for f in ${lastfilesthishour[@]}; do sed -i '$ d' $f >> $details 2>&1 ; done
-} && {
-  echo "** $datetime [ TC-STOP ] : SUCCESS : Removed last incomplete lines from ${#lastfilesthishour[@]} files."
-} || {
-  echo "** $datetime [ TC-STOP ] : ERROR : While removing last incomplete lines from files."
-} 
+chour=`date +"%H"`
+cdate=`date +"%Y-%m-%d"`
+lastfilesthishour=( `find ${lastfiles[@]} -type f -newerct "${cdate} ${chour}:00:00"` ) # confirm they were written this hour
+echo "** $datetime [ TC-STOP ] : There were ${#lastfilesthishour[@]} last files written this hour."	
 
+filesthishour=( `find ${data_dir}/*/* -type f -newerct "${cdate} ${chour}:00:00"` )
+echo "** $datetime [ TC-STOP ] : There were ${#filesthishour[@]} files written this hour."
 
-{
-  echo "** $datetime [ TC-STOP ] : Removing lasttweets beyond the specified minute range."
-  chour=`date +"%H"`
-  cdate=`date +"%Y-%m-%d"`
-  filesthishour=( `find ${data_dir}/*/* -type f -newerct "${cdate} ${chour}:00:00"` )
-  echo "** $datetime [ TC-STOP ] : There were ${#filesthishour[@]} files written this hour."
+if [[ ${#lastfilesthishour[@]} -ne 0 ]]; then
+  echo "** $datetime [ TC-STOP ] : Removing incomplete lines from last files written this hour."
+  {
+    for f in ${lastfilesthishour[@]}; do sed -i '$ d' $f >> $details 2>&1 ; done
+  } && {
+    echo "** $datetime [ TC-STOP ] : SUCCESS : Removed last incomplete lines from ${#lastfilesthishour[@]} files."
+  } || {
+    echo "** $datetime [ TC-STOP ] : ERROR : While removing last incomplete lines from files."
+  } 
+fi
 
+if [[ ${#filesthishour[@]} -ne 0 ]]; then
+
+  echo "** $datetime [ TC-STOP ] : Removing tweets beyond the specified range from files written this hour."
+  
   for f in ${filesthishour[@]}; do 
     linesatend=`cat $f | jq -r '.created_at' | grep ":1[78]:" | wc -l` 
     echo "$f: has $linesatend at end" >> $details
@@ -54,11 +56,14 @@ echo "** $datetime [ TC-STOP ] : Ended $1 at $pid"
       tail -n +${linesatstart} $f | head -n -${linesatend} > "$f.new" && mv "$f.new" "$f"
     fi  
   done
-} && {
-  echo "** $datetime [ TC-STOP ] : SUCCESS : Removed last tweets beyond the specified time range."
-} || {
-  echo "** $datetime [ TC-STOP ] : ERROR : While removing lasttweets beyond the specified minute range."
-} 
+  
+fi
+
+#} && {
+#  echo "** $datetime [ TC-STOP ] : SUCCESS : Removed last tweets beyond the specified time range."
+#} || {
+#  echo "** $datetime [ TC-STOP ] : ERROR : While removing lasttweets beyond the specified minute range."
+#} 
 
 
 #echo "** $datetime [ TC-STOP ] : Removing last line of most recent files"
